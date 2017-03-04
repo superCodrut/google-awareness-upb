947a5a40e821f5e331737784414185aef28df553
Codrut Grosu <codrut.cristian.grosu@gmail.com>
2017-03-04 23:01:57 +0200

    button: start_activity: Each button has a method
    Signed-off-by: Codrut Grosu <codrut.cristian.grosu@gmail.com>

--- app/src/main/java/cs/ai/upbassistant/Start.java
+++ app/src/main/java/cs/ai/upbassistant/Start.java
@@ -1,7 +1,10 @@
 package cs.ai.upbassistant;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class Start extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class Start extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+    }
+
+    public void startService (View view) {
+        Context context = this.getApplicationContext();
+        Toast.makeText(context, "Start", Toast.LENGTH_SHORT).show();
+    }
+    public void stopService (View view) {
+        Context context = this.getApplicationContext();
+        Toast.makeText(context, "Stop", Toast.LENGTH_SHORT).show();
+    }
+    public void exitApp (View view) {
+        Context context = this.getApplicationContext();
+        Toast.makeText(context, "Exit", Toast.LENGTH_SHORT).show();
     }
 }
