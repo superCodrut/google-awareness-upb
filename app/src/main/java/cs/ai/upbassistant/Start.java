38b1abbbc6c3bf20bdadb9704bca9b99e4fdbc45
Codrut Grosu <codrut.cristian.grosu@gmail.com>
2017-03-05 00:06:28 +0200

    alarm: start_activity: The buttons are tied to the alarm
    When the "Start Service" button is pressed, the alarm is activated and
    when the "Stop Service" button is pressed, the alarm is deactivated.
    
    Signed-off-by: Codrut Grosu <codrut.cristian.grosu@gmail.com>

--- app/src/main/java/cs/ai/upbassistant/Start.java
+++ app/src/main/java/cs/ai/upbassistant/Start.java
@@ -1,5 +1,6 @@
 package cs.ai.upbassistant;
 
+import android.app.PendingIntent;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,18 +9,26 @@ import android.widget.Toast;
 
 public class Start extends AppCompatActivity {
 
+    private AlarmService alarm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        alarm = new AlarmService();
     }
 
     public void startService (View view) {
         Context context = this.getApplicationContext();
+        if (alarm != null) {
+            alarm.setAlarm(context);
+        }
         Toast.makeText(context, "Start", Toast.LENGTH_SHORT).show();
     }
     public void stopService (View view) {
         Context context = this.getApplicationContext();
+        if (alarm != null) {
+            alarm.cancelAlarm(context);
+        }
         Toast.makeText(context, "Stop", Toast.LENGTH_SHORT).show();
     }
     public void exitApp (View view) {
