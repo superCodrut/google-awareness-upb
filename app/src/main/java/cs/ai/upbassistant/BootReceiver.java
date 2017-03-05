c9169d166fa6f46b1e674711c081359169da11cb
Codrut Grosu <codrut.cristian.grosu@gmail.com>
2017-03-05 16:16:24 +0200

    alarm: BootReceiver: The app will receive an action after the system finishes booting.
    When the ACTION_BOOT_COMPLETED action is received by the onReceive()
    method, the app will start the alarm.
    
    The alarm will start again only if the user has started the app at
    least once.
    
    Signed-off-by: Codrut Grosu <codrut.cristian.grosu@gmail.com>

---
+++ app/src/main/java/cs/ai/upbassistant/BootReceiver.java
@@ -0,0 +1,18 @@
+package cs.ai.upbassistant;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
+
+public class BootReceiver extends BroadcastReceiver {
+    AlarmService alarm = new AlarmService();
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
+            alarm.setAlarm(context);
+        }
+    }
+}
