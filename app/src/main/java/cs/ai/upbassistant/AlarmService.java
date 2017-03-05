9ae1bf04aa03180a6e9eee774a69c65066fabdbd
Codrut Grosu <codrut.cristian.grosu@gmail.com>
2017-03-05 16:32:40 +0200

    alarm: AlarmService: The alarm will automatically restart after reboot
    This happens only if the alarm was activated before the reboot.
    
    Signed-off-by: Codrut Grosu <codrut.cristian.grosu@gmail.com>

--- app/src/main/java/cs/ai/upbassistant/AlarmService.java
+++ app/src/main/java/cs/ai/upbassistant/AlarmService.java
@@ -3,8 +3,10 @@ package cs.ai.upbassistant;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.provider.Settings;
 import android.widget.Toast;
 
@@ -22,6 +24,16 @@ public class AlarmService extends BroadcastReceiver {
         // 10 de secunde
         int minutes = 1000 * 10;
         alarm.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(),minutes, pendingIntent );
+
+        /*
+         * Enable to automatically restart the alarm when the device is rebooted
+         */
+        ComponentName receiver = new ComponentName(context, BootReceiver.class);
+        PackageManager pm = context.getPackageManager();
+
+        pm.setComponentEnabledSetting(receiver,
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                PackageManager.DONT_KILL_APP);
     }
 
     public void cancelAlarm(Context context) {
